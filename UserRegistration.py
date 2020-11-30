8e7173ca2f12b8fc565eba59fba25b021af8b972
malibharti5@gmail.com <malibharti5@gmail.com>
2020-11-30 19:12:17 +0530

    Bharti-MODIFY : order of Excecuting statements is changed

--- UserRegistration.py
+++ UserRegistration.py
@@ -15,9 +15,10 @@ if __name__ == '__main__':
     user = UserRegistration()
     name_regex = "^[A-Z][a-z]{2,}"
     email_regex = "^\w{3,}(\.\w{3,})*\@[a-z]{2,}\.[a-z]{2,3}(\.[a-z]{2})*$"
+
     first_name = input("Enter your first name : ")
-    last_name = input("Enter your last name : ")
-    email = input("Enter your emailId : ")
     print(user.inputValidation(first_name, name_regex))
+    last_name = input("Enter your last name : ")
     print(user.inputValidation(last_name, name_regex))
+    email = input("Enter your emailId : ")
     print(user.inputValidation(email, email_regex))
