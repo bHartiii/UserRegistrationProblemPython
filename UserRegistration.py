ce90905d12f6fa5d2e7964a4e00d86682ae7d224
malibharti5@gmail.com <malibharti5@gmail.com>
2020-11-30 18:06:25 +0530

    Bharti-ADD : Email validation is added

--- UserRegistration.py
+++ UserRegistration.py
@@ -2,17 +2,22 @@ import re
 
 
 class UserRegistration:
-    def validateName(self, name):
-        match = re.search("^[A-Z][a-z]{2,}", name)
+
+    def inputValidation(self, string, regex):
+        match = re.search(regex, string)
         if match:
-            return name
+            return string
         else:
             return "Not Valid"
 
 
 if __name__ == '__main__':
     user = UserRegistration()
+    name_regex = "^[A-Z][a-z]{2,}"
+    email_regex = "^\w{3,}(\.\w{3,})*\@[a-z]{2,}\.[a-z]{2,3}(\.[a-z]{2})*$"
     first_name = input("Enter your first name : ")
     last_name = input("Enter your last name : ")
-    user.validateName(first_name)
-    user.validateName(last_name)
+    email = input("Enter your emailId : ")
+    print(user.inputValidation(first_name, name_regex))
+    print(user.inputValidation(last_name, name_regex))
+    print(user.inputValidation(email, email_regex))
