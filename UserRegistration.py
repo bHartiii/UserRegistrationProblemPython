d63024fd3b8aeba9f89564b9444fac3e93930d9c
malibharti5@gmail.com <malibharti5@gmail.com>
2020-11-30 19:34:16 +0530

    Bharti-ADD : Mobile number validation is added

--- UserRegistration.py
+++ UserRegistration.py
@@ -4,6 +4,12 @@ import re
 class UserRegistration:
 
     def inputValidation(self, string, regex):
+        """
+        matching string with regex
+        :param string:
+        :param regex:
+        :return string or "not valid" message:
+        """
         match = re.search(regex, string)
         if match:
             return string
@@ -13,8 +19,9 @@ class UserRegistration:
 
 if __name__ == '__main__':
     user = UserRegistration()
-    name_regex = "^[A-Z][a-z]{2,}"
+    name_regex = "^[A-Z][a-z]{2,}$"
     email_regex = "^\w{3,}(\.\w{3,})*\@[a-z]{2,}\.[a-z]{2,3}(\.[a-z]{2})*$"
+    phone_regex = "^91 [6-9][0-9]{9}$"
 
     first_name = input("Enter your first name : ")
     print(user.inputValidation(first_name, name_regex))
@@ -22,3 +29,5 @@ if __name__ == '__main__':
     print(user.inputValidation(last_name, name_regex))
     email = input("Enter your emailId : ")
     print(user.inputValidation(email, email_regex))
+    phone_no = input("Enter your phone number : ")
+    print(user.inputValidation(phone_no, phone_regex))
