ff4a20d2c0579bb89b7b258d4874d493dd93bb7b
malibharti5@gmail.com <malibharti5@gmail.com>
2020-11-30 16:15:44 +0530

    Bharti-ADD : First name validation is added

--- UserRegistration.py
+++ UserRegistration.py
@@ -1,1 +1,16 @@
-print("Welcome to user registration program")
+import re
+
+
+class UserRegistration:
+    def takeUserInput(self):
+        first_name = input("Enter your first name : ")
+        match = re.search("^[A-Z][a-z]{2,}", first_name)
+        if match:
+            return first_name
+        else:
+            return "Not Valid"
+
+
+if __name__ == '__main__':
+    user = UserRegistration()
+    print(user.takeUserInput())
